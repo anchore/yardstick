3f8e86dfab7af13ae6aeb33fb58b4177f5d80626
Weston Steimel <weston.steimel@anchore.com>
2022-11-23 14:20:21 +0000

    feat: support loading yardstick tools as plugins (#5)
    Supports loading yardstick tool plugins registered to the `yardstick.plugins.tools` entrypoint group.
    
    Signed-off-by: Weston Steimel <weston.steimel@anchore.com>

---
+++ src/yardstick/tool/plugin.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+
+
+def load_plugins():
+    tool_plugins = entry_points(group="yardstick.plugins.tools")
+    logging.warning(tool_plugins)
+
+    for tool in tool_plugins:
+        try:
+            logging.info(f"Loading tool plugin {tool.name}")
+            tool.load()
+        except:  # pylint: disable=bare-except
+            logging.exception(f"Failed loading tool plugin {tool.name}")
